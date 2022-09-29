322d6c148c4d77ca291816dbcffd7e8cc09aa903
dabauum221 <daniel.m.baum@gmail.com>
2022-09-29 13:56:10 -0400

    1 file modified

--- src/app/observable-operator/observable-operator.component.ts
+++ src/app/observable-operator/observable-operator.component.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {map} from 'rxjs/operators';
+import { Component, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observable-operator',
   templateUrl: './observable-operator.component.html',
-  styleUrls: ['./observable-operator.component.css']
+  styleUrls: ['./observable-operator.component.css'],
 })
-export class ObservableOperatorComponent {
-
+export class ObservableOperatorComponent implements OnDestroy {
   continentSelect = new FormControl();
   currentSelection: string;
+  subscription: Subscription;
 
   constructor() {
-    this.continentSelect.valueChanges.pipe(
-      map(continent => continent.toUpperCase().slice(0, 3))
-    ).subscribe((newValue) => {
-      this.currentSelection = newValue;
-    });
+    this.subscription = this.continentSelect.valueChanges
+      .pipe(map((continent) => continent.toUpperCase().slice(0, 3)))
+      .subscribe((newValue) => {
+        this.currentSelection = newValue;
+      });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
